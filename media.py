b23bfb0b56210fe6f8f564fadef1b04890d6a0e9
ENZO HENRIQUES SAROA <25254178@educ123.sp.senai.br>
2025-11-05 16:37:51 -0300

    Média aritmética de duas notas

---
+++ media.py
@@ -0,0 +1,7 @@
+print("Para calcular a média aritmética de suas notas digite logo a seguir:")
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+media = (nota1 + nota2) / 2
+print(f"A média aritmética das duas notas é: {media:.2f}")
+
+
